6ec4a88ecc1b56943deafb532862812bba573fe3
Vincent Lamoly <vincent.lanoly@gmail.com>
2023-03-13 22:21:59 -0400

    adjusting text

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Apprenez React/i);
   expect(linkElement).toBeInTheDocument();
 });
 
